5f9fa781b1d400775b6950e79576fa587949b08e
Daniel Bendel <db-183061@hs-weingarten.de>
2020-07-02 17:53:37 +0200

    resolved issues in favourite service

--- foodly/src/services/FavouriteService.js
+++ foodly/src/services/FavouriteService.js
@@ -3,24 +3,24 @@ import axios from 'axios'
 const path = 'http://localhost:8080/favourites'
 
 export default {
-  addFavourite(favourite, success) {
+  addFavourite (favourite, success) {
     axios.post(path, favourite).then(response => {
       success(response.data)
     })
   },
-  updateFavourite(favourite, success) {
+  updateFavourite (favourite, success) {
     axios.put(path + '/' + favourite.idMeal, favourite).then(response => {
       success(response.data)
     })
   },
-  getFavourites(success) {
+  getFavourites (success) {
     axios.get(path).then(response => {
       success(response.data)
     })
   },
-  removeFavourite(favorite, success) {
+  removeFavourite (favorite, success) {
     axios.delete(path + '/' + favorite.idMeal).then(response => {
       success(response.data)
     })
-   }
+  }
 }
